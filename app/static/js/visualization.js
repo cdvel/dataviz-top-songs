e29673053c2270001eb24f5cb0239adf163a284b
cesarvelandia <cesar@velandia.co>
2015-08-01 00:04:30 +0800

    Implement 3 visualizations

---
+++ app/static/js/visualization.js
@@ -0,0 +1,82 @@
+queue()
+	.defer(d3.json, "/top-songs/api/v1.0/songs")
+	.await(visualize);
+
+function visualize(error, data){
+	var top_songs = data["songs"];
+	var formatter = d3.time.format("%Y");
+
+	top_songs.forEach(function(song){
+		song["year"] = formatter.parse(song["year"]);
+		song["year"].setDate(1);
+	});
+
+	xfilter = crossfilter(top_songs);
+	var year_dimension =  xfilter.dimension(function (song){
+		return song["year"];
+	});
+	var artist_dimension =  xfilter.dimension(function (song){
+		return song["artist"];
+	});	
+	
+	var theme_dimension =  xfilter.dimension(function (song){
+
+		return song["theme"].replace(" and ", " & ");
+
+	});	
+
+
+	var song_count_by_year = year_dimension.group();
+	var song_count_by_theme = theme_dimension.group();
+	var song_count_by_artist = artist_dimension.group();
+
+	//workaround to limitaton of dc.js
+	function fake_top(source_group, n) {
+	    return {
+	        all: function () {
+	            return source_group.top(Infinity)
+	                .slice(0, n);
+	        }
+	    };
+	}
+
+	var song_count_by_artist_top_five = fake_top(song_count_by_artist, 5);
+
+	var year_origin = year_dimension.bottom(1)[0]["year"];
+	var year_end = year_dimension.top(1)[0]["year"];
+
+	var yearly_chart = dc.barChart("#yearly-bar-chart");
+	var themes_chart = dc.pieChart("#themes-pie-chart");
+	var artist_chart = dc.rowChart("#artist-row-chart");
+
+	yearly_chart
+		.width(818)
+		.height(240)
+		.margins({top: 10, right: 50, bottom: 30, left: 50})
+		.dimension(year_dimension)
+		.group(song_count_by_year)
+		.transitionDuration(500)
+		.x(d3.time.scale().domain([year_origin, year_end]))
+		.elasticY(true)
+		.xAxisLabel("Year")
+		.yAxis().ticks(4);
+
+	themes_chart
+		.width(300)
+		.height(300)
+		.dimension(theme_dimension)
+		.group(song_count_by_theme)
+		.transitionDuration(500)
+		.innerRadius(60)
+
+	artist_chart
+		.width(393)
+		.height(240)
+        .dimension(artist_dimension)
+        .group(song_count_by_artist_top_five)
+        .xAxis().ticks(5);
+
+    dc.renderAll();
+
+
+} //end visualize
