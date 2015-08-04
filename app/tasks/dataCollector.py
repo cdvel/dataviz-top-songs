7fe254fa23147c2ffde4c00b6121c2a92a91ac41
cesarvelandia <cesar@velandia.co>
2015-08-04 20:04:38 +0800

    Improve error handling and update all songs bug fix

--- app/tasks/dataCollector.py
+++ app/tasks/dataCollector.py
@@ -1,29 +1,26 @@
 import requests, re
 from config import LASTFM_API
 
-BASE_URL = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo"
-API_KEY = "1f25dd6f5bd821261580e89974d7c246"
 
 # Collects song stats from an external API
 class DataCollector:
-
 	# Returns listeners and playcount for artist-song pair
-	def  getTrackStats(self, artist_name, song_name):
+	def  get_track_stats(self, artist_name, song_name):
 		
-		artist_name = self.normalizer(artist_name)
-		song_name = self.normalizer(song_name)
+		artist_name = self.normalize(artist_name)
+		song_name = self.normalize(song_name)
 
-		payload = {'api_key': LASTFM_API['key'], 'artist': artist_name,'track': song_name, 'format': 'json'}
-		response = requests.get(LASTFM_API['track_url'],  params=payload)
+		payload = {'api_key': LASTFM_API['key'], 'artist': artist_name, 'track': song_name, 'format': 'json'}
+		response = requests.get(LASTFM_API['track_url'], params=payload)
 
 		assert response.status_code == 200
 		payload = response.json()
 
-		stats = {'listeners': 0,'playcount': 0}
+		stats = {'listeners': 0, 'playcount': 0}
 
 		# a match wasn't found; default to 0
 		if 'message' in payload:
-			print  "%r - %r : %r"%(artist_name, song_name, payload['message'])
+			print "%r - %r : %r"%(artist_name, song_name, payload['message'])
 
 		else:
 			# the service may return empty strings
@@ -36,7 +33,7 @@ class DataCollector:
 		return stats
 
 	# Improves remote calls matches
-	def normalizer(self, s):
+	def normalize(self, s):
 		normal = ""
 		blist = ["I", "We"]
 		for w in s.split():
